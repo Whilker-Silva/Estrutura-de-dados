1600459a4f9c2e135f62268cdbdfe591854d106b
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2024-03-14 19:50:13 -0300

    Aitividade pilha

---
+++ 01 Tads/Aulão 1/Teste.h
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Teste
+{
+private:
+    int x;
+
+public:
+    Teste(/* args */);
+    ~Teste();
+};
+
+Teste::Teste(/* args */)
+{
+    x = 1;
+}
+
+Teste::~Teste()
+{
+}
